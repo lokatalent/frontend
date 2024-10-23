3f7d706aa4d3854991e1962ced01019cc73d0ca2
Tolanee <shodunkeomotolani@gmail.com>
2024-10-24 00:18:32 +0100

    build fail fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
+import Footer from "@/components/Footer";
 import ApplyHere from "@/components/home/ApplyHere";
 import Hero from "@/components/home/Hero";
 import HowItWorks from "@/components/home/HowItWorks";
-import Navbar from "@/components/Nav";
-import Testimonial from "../components/home/Testimonial";
-import Footer from "@/components/Footer";
 import Preview from "@/components/home/Preview";
+import Testimonial from "../components/home/Testimonial";
 
 export default function Home() {
 	return (
 		<div>
-			
 			<Hero />
 			<Preview />
 			<HowItWorks />
