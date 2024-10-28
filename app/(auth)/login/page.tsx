b351b6e08587b0155944effbf44c30db6df8957e
KEHINDE ADIGUN <keahnney01@gmail.com>
2024-10-28 11:13:38 +0100

    customer auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,84 @@
-import AuthForm from "@/components/auth/AuthForm";
+"use client"
+import DynamicForm from "@/components/ui/form/DynamicForm";
+import { FieldConfig, LogInFormSchema, SignUpFormSchema } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
 
 const LogIn = () => {
-    return <AuthForm type="log-in" />;
+    const fields: FieldConfig[] = [
+      {
+        name: "email",
+        type: "email",
+        label: "Email",
+        validation: {
+          required: "Email is required",
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: "Invalid email format",
+          },
+        },
+      },
+      {
+        name: "password",
+        type: "password",
+        label: "Password",
+        validation: {
+          required: "Password is required",
+          minLength: {
+            value: 6,
+            message: "Password must be at least 6 characters",
+          },
+          pattern: {
+            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+            message: "Password must contain at least one letter and one number",
+          },
+        },
+      },
+    ];
+
+    const defaultValues = {
+        email: "aaa",
+        password: '',
+    };
+
+    const schemaType = LogInFormSchema;
+    return (
+      <div className="w-8/12 mx-auto mt-20 mb-6 space-y-6">
+        <div className="text-center space-y-2">
+          <h1 className="font-bold text-4xl text-textColor py-4">
+            Welcome Back
+          </h1>
+          <p className="w-1/2 mx-auto">
+
+            Please enter your details to be able to lorem ipsum lorem ipsum lorem ipsium
+              
+          </p>
+        </div>
+        
+        <DynamicForm
+          fields={fields}
+          defaultValues={defaultValues}
+          schemaType={schemaType}
+          buttonAction="log-in"
+        />
+
+        
+          <footer className="flex justify-center gap-2 ">
+            <p className="text-sm font-normal text-gray-600">
+              
+                Dont have an account?
+                
+            </p>
+            <Link
+              href={"/signup"}
+              className="text-primaryBlue text-sm"
+            >
+              Sign Up
+            </Link>
+          </footer>
+        
+      </div>
+    );
 };
 
 export default LogIn;
--- components/ui/formNew.tsx
+++ components/ui/formNew.tsx
@@ -147,10 +147,11 @@ const FormMessage = React.forwardRef<
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, children, ...props }, ref) => {
   const { error, formMessageId } = useFormField()
+  
   const body = error ? String(error?.message) : children
 
   if (!body) {
-    return null
+    return <p>a</p>
   }
 
   return (
