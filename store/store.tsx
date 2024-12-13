79541056c6ccfb6967cfac4b4fceed80a0f5c84b
KEHINDE ADIGUN <keahnney01@gmail.com>
2024-12-13 09:44:11 +0100

    talent dashboard 2

--- store/store.tsx
+++ store/store.tsx
@@ -2,10 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "./profile/profileSlice";
 import settingsReducer from "./settings/SettingsSlice";
+import talentProfileReducer from "./talent/profile/TalentProfileSlice";
 
 export const store = configureStore({
   reducer: {
     profile: profileReducer,
     settings: settingsReducer,
+    talentProfile: talentProfileReducer,
   },
 });
