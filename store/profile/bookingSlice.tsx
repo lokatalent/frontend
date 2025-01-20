df9cd0dbb053becfbb1950d413db8ce360a5a914
chrismadufor <chrismadufor@gmail.com>
2025-01-20 11:26:49 +0100

    update

--- store/profile/bookingSlice.tsx
+++ store/profile/bookingSlice.tsx
@@ -1,31 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    service: '',
-    location: '',
-    allServices: '',
-}
+  service: "",
+  location: "",
+  bookingData: "",
+  allServices: "",
+};
 
 const bookingSlice = createSlice({
-    name: 'booking',
-    initialState,
-    reducers: {
-      setBookingService: (state, {payload}) => {
-        state.service = payload;
-      },
-      setBookingLocation: (state, {payload}) => {
-        state.location = payload;
-      },
-      setAllServices: (state, {payload}) => {
-        state.allServices = payload;
-      },
-      resetBooking: (state) => {
-        state.service = "";
-        state.location = "";
-        state.allServices = "";
-      },
-    }
-  })
+  name: "booking",
+  initialState,
+  reducers: {
+    setBookingService: (state, { payload }) => {
+      state.service = payload;
+    },
+    setBookingLocation: (state, { payload }) => {
+      state.location = payload;
+    },
+    setBookingData: (state, { payload }) => {
+      state.bookingData = payload;
+    },
+    setAllServices: (state, { payload }) => {
+      state.allServices = payload;
+    },
+    resetBooking: (state) => {
+      state.service = "";
+      state.location = "";
+      state.allServices = "";
+    },
+  },
+});
 
-  export const {setBookingLocation, setBookingService, setAllServices, resetBooking} = bookingSlice.actions
-  export default bookingSlice.reducer
+export const {
+  setBookingLocation,
+  setBookingService,
+  setBookingData,
+  setAllServices,
+  resetBooking,
+} = bookingSlice.actions;
+export default bookingSlice.reducer;
