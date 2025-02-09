90ef3dd2d5dae76d45ccf2cd3149457cc30ff5b4
Paul Oyelabi <oyelabipaul@gmail.com>
2025-02-09 11:50:58 +0100

    fix: updated `bookingData` state member type

--- store/profile/bookingSlice.tsx
+++ store/profile/bookingSlice.tsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   service: "",
   location: "",
-  bookingData: "",
+  bookingData: {},
   talentData: "",
   allServices: "",
 };
