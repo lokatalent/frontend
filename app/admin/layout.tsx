8f50782d1066cac8764a8b9c62115597bc86c97a
Paul Oyelabi <oyelabipaul@gmail.com>
2025-02-12 22:19:30 +0100

    chore: restricted access to admin panel

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,26 +1,36 @@
+"use client";
+
 import TopNav from "@/components/dash/TopNav";
 import SideNav from "@/components/dash/SideNav";
+import { useSelector } from "react-redux";
 
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const loggedIn = true;
+  const currentUserRole = useSelector((state: any) => state.auth.user.role);
+	// const loggedIn = true;
 
 	// if (!loggedIn) redirect("/login");
 	return (
-		<div className=" main-contain ">
-			<div className="flex flex-row">
-				<div className="sidenav-container">
-					<SideNav />
-				</div>
-				<div className="flex-1 md:ml-[200px]">
-					<TopNav />
-				</div>
-			</div>
-
-			<div className="main-content xl:pl-[200px] mx-8">{children}</div>
-		</div>
+    currentUserRole.startsWith("admin") ?
+      (
+        <div className=" main-contain ">
+			    <div className="flex flex-row">
+            <div className="sidenav-container">
+              <SideNav />
+            </div>
+            <div className="flex-1 md:ml-[200px]">
+              <TopNav />
+            </div>
+          </div>
+          <div className="main-content xl:pl-[200px] mx-8">{children}</div>
+		    </div>
+    ) : (
+      <div className=" main-contain ">
+        <div>Unauthorized</div>
+      </div>
+    )
 	);
 }
