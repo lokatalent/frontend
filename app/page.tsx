48c626bab27d61f5131fc10a3cf82d5d30b3c630
Paul Oyelabi <oyelabipaul@gmail.com>
2025-02-26 00:43:08 +0100

    chore: updated routes
        -   changed the root route to the landing page where user can
            either sign-in or sign-up or book a service.
    
        -   updated waiting list route to /waitlist.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,43 @@
-import Waitlist from "@/components/home/Waitlist";
+"use client"
+
+import Footer from "@/components/Footer";
+import ApplyHere from "@/components/home/ApplyHere";
+import Hero from "@/components/home/Hero";
+import HowItWorks from "@/components/home/HowItWorks";
+import Testimonial from "@/components/home/Testimonial";
+import Preview from "@/components/home/Preview";
 import Navbar from "@/components/Nav";
+import { getServices } from "@/services/bookingService";
+import { showToast } from "@/store/auth/toastSlice";
+import { setAllServices } from "@/store/profile/bookingSlice";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
+  const fetchServices = async () => {
+    setLoading(true);
+    const response = await getServices();
+    if (!response.error) {
+      setLoading(false);
+      dispatch(setAllServices(response.data));
+    } else {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    fetchServices()
+  }, [])
   return (
     <div>
-      <Navbar waitlist={true} />
-      <div>
-        <Waitlist />
-      </div>
+      <Navbar />
+      <Hero />
+      <Preview />
+      <HowItWorks />
+      <ApplyHere />
+      <Footer />
     </div>
   );
 }
